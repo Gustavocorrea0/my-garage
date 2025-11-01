8f845000bcf5dbc2ea122c6546c968fad7a1e0c7
Gustavocorrea0 <gustavocorreaofc@gmail.com>
2025-11-01 17:07:49 -0300

    create readme.md

--- app/src/main/java/com/example/gustavo/mygaragev2/model/DataStore.kt
+++ app/src/main/java/com/example/gustavo/mygaragev2/model/DataStore.kt
@@ -7,17 +7,20 @@ object DataStore {
 
     // informacoes iniciais
     init {
-        cars.add(Car("RS3", "Audi", "AAA1A111", 2019,
-            "Metallic Gray","Gasoline",422000.0,434999.9,
-            "https://motorshow.com.br/wp-content/uploads/sites/2/2019/01/10_ms423_audi-rs-3-sportback1.jpg"))
+        cars.add(Car("RB21", "Red Bull/Honda", "MVD2X42", 2021,
+            "White","Gasoline",3500000.0,3500000.0,
+            "https://www.minimundi.com.br/cdn/imagens/produtos/det/miniatura-formula-1-oracle-red-bull-racing-rb19-1-max-verstappen-2023-1-43-burago-c-acrilico-38083-b.jpeg"
 
-        cars.add(Car("A45 AMG", "Mercedes-Benz", "BBB1B111", 2020,
-            "Red","Gasoline", 359070.0,379070.0,
-            "https://garagemde0a100.com.br/wp-content/uploads/2024/11/mercedes-amg-a-45-4matic-2019-1.jpg"))
+            ))
 
-        cars.add(Car("Astra GSI 16V", "Chevrolet", "CCC1C111",
-            0, "Red Apple","Gasoline",50000.0,55000.0,
-            "https://i.ytimg.com/vi/sEYpa8GfwGY/maxresdefault.jpg"))
+        cars.add(Car("RS3", "Audi", "AAA2D23", 2019,
+            "Metallic Gray","Gasoline", 422000.0,434999.0,
+            "https://cdn.awsli.com.br/2500x2500/2571/2571273/produto/284644765/gt434-fl-ctkws0dsre.jpg"
+            ))
+
+        cars.add(Car("911 GT3 RS", "Porsche", "FDA2D24",
+            2021, "White","Gasoline",1500000.0,1700000.0,
+            "https://cdn.awsli.com.br/2500x2500/1302/1302593/produto/237201775/t64mc-005-wr_a-mweogkhq9l.jpg"))
     }
 
     // retornar carro
---
+++ readme.md
@@ -0,0 +1,43 @@
+# My Garage - Android App
+
+### Description
+
+- App responsible for storing information about a collection of miniatures or cars.
+
+### Developer
+
+- Gustavo Alfredo Correa Da Silva
+
+### Course information
+
+- University: PUC Paraná - Curitiba
+- Postgraduate Course: Mobile Application Development
+- Course: Android Application Development
+
+
+> 🔨 Project Functions:
+
+    - Create: Add a New Car/Miniature.
+    - Read All: View All Registered Cars/Miniatures.
+    - Read One: View a Single Car/Miniature.
+    - Update: Update a Car/Miniature.
+    - Delete: Delete a Single Car/Miniature.
+
+> 🔨 Technical Information:
+
+    - Programming Language: Kotlin Android.
+    - Storage: Local (Device).
+    - Design Pattern: MVC (Model, View, Controller).
+
+
+### Tools
+
+IDEA: [Android Studio](https://developer.android.com/studio?hl=pt-br) &nbsp;&middot;&nbsp; <br>
+Prototipagem de Telas: [Figma](https://www.figma.com/pt-br/) &nbsp;&middot;&nbsp; <br>
+
+### Images
+
+## 📷 Telas
+
+<img src="./img-screen/1login.png" alt="Alt text" width="200"/><img src="./img-screen/2home.png" alt="Alt text" width="200"/>
+<img src="./img-screen/3create.png" alt="Alt text" width="200"/><img src="./img-screen/4RUD.png" alt="Alt text" width="200"/>
